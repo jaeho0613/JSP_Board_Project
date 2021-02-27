cd546ed1f38dd17932ff5ffe4151c9fab89b2f80
Jaeho Jeong <ywm2004@gmail.com>
2021-02-27 13:40:45 +0900

    [ 화요일 평가 시험 초기 설정 ] - DAO, DTO - Elipse 서버 설정 - MySql Connection Libary - Database Util

---
+++ Board/src/modle/BoardDTO.java
@@ -0,0 +1,56 @@
+package modle;
+
+import java.sql.Date;
+
+public class BoardDTO {
+	int boardNum;
+	String boardTitle;
+	String boardAuthor;
+	Date boardCreateDate;
+	int boardViews;
+
+	public int getBoardNum() {
+		return boardNum;
+	}
+
+	public BoardDTO setBoardNum(int boardNum) {
+		this.boardNum = boardNum;
+		return this;
+	}
+
+	public String getBoardTitle() {
+		return boardTitle;
+	}
+
+	public BoardDTO setBoardTitle(String boardTitle) {
+		this.boardTitle = boardTitle;
+		return this;
+	}
+
+	public String getBoardAuthor() {
+		return boardAuthor;
+	}
+
+	public BoardDTO setBoardAuthor(String boardAuthor) {
+		this.boardAuthor = boardAuthor;
+		return this;
+	}
+
+	public Date getBoardCreateDate() {
+		return boardCreateDate;
+	}
+
+	public BoardDTO setBoardCreateDate(Date boardCreateDate) {
+		this.boardCreateDate = boardCreateDate;
+		return this;
+	}
+
+	public int getBoardViews() {
+		return boardViews;
+	}
+
+	public BoardDTO setBoardViews(int boardViews) {
+		this.boardViews = boardViews;
+		return this;
+	}
+}
